a5b42d9ffb6b3b9e00212133f02da0d8d0f41eb7
OscarFridh <oscar.fridh.mkk@gmail.com>
2019-03-19 15:48:00 +0100

    Added a collectionView

--- Concentration.playground/Contents.swift
+++ Concentration.playground/Contents.swift
@@ -4,17 +4,29 @@ import UIKit
 import PlaygroundSupport
 
 class MyViewController : UIViewController {
-    override func loadView() {
-        let view = UIView()
-        view.backgroundColor = .white
-
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
+    
+//    private var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        view.addSubview(label)
-        self.view = view
+        view.backgroundColor = .white
+        
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        
+//        let collectionView = UIView()
+        collectionView.backgroundColor = .red
+        
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        
+//        print(collectionView.frame)
     }
 }
 // Present the view controller in the Live View window
