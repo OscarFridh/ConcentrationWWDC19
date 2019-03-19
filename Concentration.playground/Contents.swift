efcd3a4def2f38cb77ca17c652eacfd287c6a9da
OscarFridh <oscar.fridh.mkk@gmail.com>
2019-03-19 16:10:58 +0100

    Adjusted the appearance of the collectionView and the test cells

--- Concentration.playground/Contents.swift
+++ Concentration.playground/Contents.swift
@@ -7,6 +7,10 @@ class MyViewController : UIViewController {
     
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        layout.minimumLineSpacing = 20
+        layout.minimumInteritemSpacing = 20
+        layout.itemSize = CGSize(width: 60, height: 60)
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = #colorLiteral(red: 0.07843137255, green: 0.09803921569, blue: 0.1843137255, alpha: 1)
         return collectionView
@@ -38,6 +42,7 @@ extension MyViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         cell.backgroundColor = .white
+        cell.layer.cornerRadius = cell.frame.width/4
         return cell
     }
 }
