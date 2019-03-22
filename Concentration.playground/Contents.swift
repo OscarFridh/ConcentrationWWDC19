7ad327696085c38c6465a95983263b930054d643
OscarFridh <oscar.fridh.mkk@gmail.com>
2019-03-22 16:57:04 +0100

    Extended ChineseGlossaries and moved it to its own file

--- Concentration.playground/Contents.swift
+++ Concentration.playground/Contents.swift
@@ -2,19 +2,9 @@
   
 import PlaygroundSupport
 
-
 let viewController = ConcentrationViewController()
-viewController.cards = [
-    "好",
-    "你",
-    "很",
-    "我",
-    "她"
-]
+viewController.cards = ChineseGlossaries.chapter1
 viewController.voice = Voice(language: .chinese)
-
-//let resultsVC = ResultsViewController()
-//resultsVC.gameState = GameState(["A", "B", "C"])
 
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = viewController
